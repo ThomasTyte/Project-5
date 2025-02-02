8e31dc1f1fde4488e78519f9197b8299521e422a
thomas <tom_tyte@hotmail.com>
2025-02-01 21:57:16 -0500

    update render

---
+++ server/my-express-app/src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'; // Use this if you're using ES modules
+// const express = require('express'); // Use this line if you're using CommonJS
+
+const app = express();
+// Use the dynamic port provided by Render, or fallback to 3001 if not defined (for local testing)
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello, World! This is served by the Express server!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
