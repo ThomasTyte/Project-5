1acf5879ca8c85a309c358c8ffa4beba4036d091
thomas <tom_tyte@hotmail.com>
2025-02-02 20:33:42 -0500

    railway

--- server/my-express-app/src/index.js
+++ server/my-express-app/src/index.js
@@ -3,7 +3,7 @@ import express from 'express'; // Use this if you're using ES modules
 
 const app = express();
 // Use the dynamic port provided by Render, or fallback to 3001 if not defined (for local testing)
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello, World! This is served by the Express server!' });
